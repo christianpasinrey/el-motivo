81dcb3fa948dd1a2a3ce4067f30f236fbe3e747d
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2026-07-03 22:59:36 +0200

    feat: scaffold Astro with design tokens and base layout

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://christianpasinrey.github.io',
+  base: '/el-motivo',
+  integrations: [sitemap()],
+});
